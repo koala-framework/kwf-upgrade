3f52dcaf48ecb5175fed462c4b9e4b4ac4803ae7
Niko Sams <ns@vivid-planet.com>
2014-12-11 18:19:37 +0100

    add new script to upgrade to master

---
+++ upgrade-to-master.php
@@ -0,0 +1,23 @@
+<?php
+if (is_file('vkwf_branch') || is_file('kwf_branch')) {
+    die("This script will update from 3.8, update to 3.8 first.\n");
+}
+if (!is_file('composer.json')) {
+    die("composer.json not found.\n");
+}
+
+$changed = false;
+$c = json_decode(file_get_contents('composer.json'));
+foreach ($c->require as $packageName=>$packageVersion) {
+    if (substr($packageVersion, 0, 4) == "3.8.") {
+        $c->require->$packageName = 'dev-master';
+        $changed = true;
+    }
+}
+if (!$changed) {
+    die("This script will update from 3.8, update to 3.8 first.\n");
+}
+file_put_contents('composer.json', json_encode($c, (defined('JSON_PRETTY_PRINT') ? JSON_PRETTY_PRINT : 0) + (defined('JSON_UNESCAPED_SLASHES') ? JSON_UNESCAPED_SLASHES : 0) ));
+
+echo "\n";
+echo "run now 'composer update' to update dependencies\n";
