d11c3361af3a6c55255ab10b445e292d76ac9294
Benjamin Hohenwarter <bh@vivid-planet.com>
2015-06-15 08:34:29 +0200

    Corrected paths for trl upgrade part in 3.9 script

--- upgrade-to-3.9.php
+++ upgrade-to-3.9.php
@@ -67,8 +67,8 @@ foreach ($files as $file) {
 
 //---------------------------------------------------------
 // Convert trl.xml into po files
-if (file_exists(__DIR__."/trl.xml")) {
-    $trlXmlDocument = simplexml_load_string(file_get_contents(__DIR__."/trl.xml"));
+if (file_exists("trl.xml")) {
+    $trlXmlDocument = simplexml_load_string(file_get_contents("trl.xml"));
     $languages = array();
     foreach ($trlXmlDocument->text as $trls) {
         foreach ($trls as $lang => $trl) {
@@ -78,20 +78,20 @@ if (file_exists(__DIR__."/trl.xml")) {
             $languages[$lang] = true;
         }
     }
-    if (!is_dir(__DIR__.'/trl')) mkdir(__DIR__.'/trl');
-    $config = parse_ini_file(__DIR__.'/config.ini');
+    if (!is_dir('trl')) mkdir('trl');
+    $config = parse_ini_file('config.ini');
     $webcodeLanguage = $config['webCodeLanguage'];
     foreach (array_keys($languages) as $lang) {
         passthru('php '.__DIR__.'/upgrade-to-3.9/trl convertTrlXmlToPo '
-            .'--outputPath="'.__DIR__.'/trl/'.$lang.'.po" '
+            .'--outputPath="'.getcwd().'/trl/'.$lang.'.po" '
             .'--webcodeLanguage="'.$webcodeLanguage.'" '
             .'--targetLanguage="'.$lang.'" '
-            .'"'.__DIR__.'/trl.xml"', $ret);
+            .'"'.getcwd().'/trl.xml"', $ret);
         if ($ret) exit($ret);
     }
-    unlink(__DIR__.'/trl.xml');
+    unlink('trl.xml');
     echo "Converted trl.xml into po files. Languages: ".implode(", ", array_keys($languages))."\n";
-    if (file_exists(__DIR__.'/vendor/vivid-planet/vkwf')) {
+    if (file_exists('vendor/vivid-planet/vkwf')) {
         echo "trl folder added to gitignore\n";
         file_put_contents('.gitignore', file_get_contents('.gitignore')."trl\n");
     } else {
