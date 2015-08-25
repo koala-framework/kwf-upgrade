fd7a9f5a718a706bd43dfeb8d802c0c1f2061f82
Benjamin Hohenwarter <bh@vivid-planet.com>
2015-08-25 13:46:50 +0200

    Simplify usage of convertTrlToPoFile command
    Most params shouldn't be required and lead to confusion. Now they
    are optional.

--- upgrade-to-3.9/ConvertTrlXmlToPoCommand.php
+++ upgrade-to-3.9/ConvertTrlXmlToPoCommand.php
@@ -12,25 +12,57 @@ class ConvertTrlXmlToPoCommand extends Command
     {
         $this->setName('convertTrlXmlToPo')
             ->setDescription('Convert xml-trl file into po-trl file')
-            ->addArgument('trlXmlPath', InputArgument::REQUIRED, 'Path to trl xml file')
-            ->addOption('outputPath', 'p', InputOption::VALUE_OPTIONAL, 'Filename for trl po file', 'trl.po')
-            ->addOption('webcodeLanguage', 'b', InputOption::VALUE_OPTIONAL, 'Language used as id (default is en)', 'en')
-            ->addOption('targetLanguage', 't', InputOption::VALUE_OPTIONAL, 'Language used for translation (default is none)');
+            ->addOption('webcodeLanguage', 'b', InputOption::VALUE_OPTIONAL, 'Language used as id (default looking up in config.ini)')
+            ->addOption('targetLanguage', 't', InputOption::VALUE_OPTIONAL, 'Language used for translation (if none specified every containing language is created)')
+            ->addOption('trlXmlPath', 'p', InputOption::VALUE_OPTIONAL, 'Path to trl xml file');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $trlXmlPath = $input->getArgument('trlXmlPath');
-        $outputPath = $input->getOption('outputPath');
+        $trlXmlPath = $input->getOption('trlXmlPath');
+        if (!$trlXmlPath) {
+            $trlXmlPath = 'trl.xml';
+        }
         $webcodeLanguage = $input->getOption('webcodeLanguage');
-        $targetLanguage = $input->getOption('targetLanguage');
+        if (!$webcodeLanguage) {
+            if (!file_exists('config.ini')) {
+                $output->writeln('no config.ini found to get webcodeLanguage');
+                return;
+            }
+            $config = parse_ini_file('config.ini');
+            $webcodeLanguage = $config['webCodeLanguage'];
+        }
 
+        $targetLanguage = $input->getOption('targetLanguage');
+        if (!$targetLanguage) {
+            $xmlDocument = simplexml_load_string(file_get_contents($trlXmlPath));
+            $targetLanguages = array();
+            foreach ($xmlDocument->text as $trl) {
+                foreach ($trl as $key => $value) {
+                    if ($key == 'id' || $key == 'context') continue;
+                    if (strpos($key, '_plural') !== false) continue;
+                    $targetLanguages[$key] = true;
+                }
+            }
+            $targetLanguages = array_keys($targetLanguages);
+        } else {
+            $targetLanguages = array($targetLanguage);
+        }
+
+        if (!is_dir('trl')) mkdir('trl');
+        foreach ($targetLanguages as $targetLanguage) {
+            $this->_convertTrlXmlToPo($trlXmlPath, $webcodeLanguage, $targetLanguage, $output);
+        }
+    }
+
+    private function _convertTrlXmlToPo($trlXmlPath, $webcodeLanguage, $targetLanguage, $output)
+    {
         $trlXmlToPoConverter = new TrlXmlToPoConverter();
         $trlXmlToPoConverter->setXmlPath($trlXmlPath);
         $trlXmlToPoConverter->setBaseLanguage($webcodeLanguage);
         if ($targetLanguage) $trlXmlToPoConverter->setTargetLanguage($targetLanguage);
         $trlXmlToPoConverter->convertToPo($output);
-
-        $trlXmlToPoConverter->writePoContent($outputPath);
+        $output->writeln("$targetLanguage.po-File was written to ".getcwd()."/trl/$targetLanguage.po");
+        $trlXmlToPoConverter->writePoContent("trl/$targetLanguage.po");
     }
 }
