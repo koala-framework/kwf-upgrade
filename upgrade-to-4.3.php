cf9761e33d56f25d1524f423c141bafe73b4741c
Niko Sams <ns@vivid-planet.com>
2016-12-05 10:17:30 +0100

    add script for upgrading to 4.3

--- upgrade-to-4.3.php
+++ upgrade-to-4.3.php
@@ -3,7 +3,7 @@
 require __DIR__.'/util/globrecursive.php';
 
 if (is_file('vkwf_branch') || is_file('kwf_branch')) {
-    die("This script will update from 4.3, update to 4.3 first.\n");
+    die("This script will update from 4.2, update to 4.2 first.\n");
 }
 if (!is_file('composer.json')) {
     die("composer.json not found.\n");
@@ -12,13 +12,13 @@ if (!is_file('composer.json')) {
 $changed = false;
 $c = json_decode(file_get_contents('composer.json'));
 foreach ($c->require as $packageName=>$packageVersion) {
-    if (substr($packageVersion, 0, 4) == "4.3.") {
-        $c->require->$packageName = 'dev-master';
+    if (substr($packageVersion, 0, 4) == "4.2.") {
+        $c->require->$packageName = '4.3.x-dev';
         $changed = true;
     }
 }
 if (!$changed) {
-    die("This script will update from 4.3, update to 4.3 first.\n");
+    die("This script will update from 4.2, update to 4.2 first.\n");
 }
 
 file_put_contents('composer.json', json_encode($c, (defined('JSON_PRETTY_PRINT') ? JSON_PRETTY_PRINT : 0) + (defined('JSON_UNESCAPED_SLASHES') ? JSON_UNESCAPED_SLASHES : 0) ));
